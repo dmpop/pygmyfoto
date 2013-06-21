d691da0f9d71bc822d0f133c08a95cadb0eee2e1
Dmitri Popov <dmpop@linux.com>
2013-06-21 14:02:38 +0200

    Added rss.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 $title = 'P * y * g * m * y * f * o * t * o';
-$footer='Powered by Pygmyfoto';
+$footer='Powered by Pygmyfoto. <a href="rss.php">RSS feed</a>';
 $baseurl="http://127.0.0.1/pygmyfoto/"; // Remember the trailing slash
 ?> 
