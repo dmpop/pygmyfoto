641ec697f2c8656a2305dd8f1db6f9c6bd051ddf
Dmitri Popov <dmpop@linux.com>
2013-06-21 13:59:28 +0200

    Added rss.php

--- config.php
+++ config.php
@@ -1,4 +1,5 @@
 <?php
 $title = 'P * y * g * m * y * f * o * t * o';
 $footer='Powered by Pygmyfoto';
+$baseurl="http://127.0.0.1/pygmyfoto/"; // Remember the trailing slash
 ?> 
---
+++ rss.php
@@ -0,0 +1,32 @@
+<?php
+
+$db = new PDO('sqlite:pygmyfoto.sqlite');
+
+header("Content-type: text/xml"); 
+
+echo "<?xml version='1.0' encoding='UTF-8'?> 
+<rss version='2.0'>
+<channel>
+<title>Pygmyfoto</title>
+<link>https://github.com/dmpop/pygmyfoto</link>
+<description>Pygmyfoto</description>
+<language>en-us</language>"; 
+
+$result = $db->query("SELECT id, title, tags, published FROM photos WHERE published = '1' ORDER BY dt DESC");
+
+include 'config.php';
+
+foreach ($result as $row)
+{
+	$title=$row['title'];
+	$URL=$baseurl."photo.php?id=".$row['id'];
+	$tags=$row['tags'];
+	echo "<item><title>$title</title>
+	<link>$URL</link>
+	<description>$tags</description></item>";
+}
+echo "</channel></rss>";
+
+?>
+
+
