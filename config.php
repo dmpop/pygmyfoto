4d107f58bd49d826a782c5cd0366fa3160525575
Dmitri Popov <dmpop@linux.com>
2013-07-01 12:40:30 +0200

    Added queue with view

--- config.php
+++ config.php
@@ -2,12 +2,12 @@
 
 $title = 'P * y * g * m * y * f * o * t * o';
 
-$navigation="<center><a href='index.php'>Home</a> &#10034; <a href='randomphoto.php'>Random photo</a> &#10034; <a href='archive.php'>Archive</a> &#10034; <a href='stats.php'>Stats</a> &#10034; <a href='https://github.com/dmpop/pygmyfoto'>Pygmyfoto</a></center>";
+$navigation="<center><a href='index.php'>Home</a> &#10034; <a href='randomphoto.php'>Random photo</a> &#10034; <a href='archive.php'>Archive</a> &#10034; <a href='photos/queue'>Queue</a> &#10034; <a href='stats.php'>Stats</a> &#10034; <a href='https://github.com/dmpop/pygmyfoto'>Pygmyfoto</a></center>";
 
 $footer='Powered by Pygmyfoto. <a href="rss.php">RSS feed</a>';
 
 $baseurl="http://dmpop.dyndns.org/pygmyfoto/"; // Remember the trailing slash
 
-$password="password" // For use with the publish.php and unpublish.php scripts
+$password="m0nk3y" // For use with the uploader.php, publish.php and unpublish.php scripts
 
 ?> 
