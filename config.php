e3cb703aee7cbcbb0ad7cb6bb20c5c85d55f9942
Dmitri Popov <dmpop@linux.com>
2013-07-05 10:31:55 +0200

    Renamed Queue to Stream

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 $title = 'Pygmyfoto';
 
-$navigation="<a href='index.php'>Home</a> &#10034; <a href='randomphoto.php'>Random photo</a> &#10034; <a href='archive.php'>Archive</a> &#10034; <a href='queue.php'>Queue</a> &#10034; <a href='stats.php'>Stats</a> &#10034; <a href='https://github.com/dmpop/pygmyfoto'>Pygmyfoto</a>";
+$navigation="<a href='index.php'>Home</a> &#10034; <a href='randomphoto.php'>Random photo</a> &#10034; <a href='stream.php'>Stream</a> &#10034; <a href='archive.php'>Archive</a> &#10034; <a href='stats.php'>Stats</a> &#10034; <a href='https://github.com/dmpop/pygmyfoto'>Pygmyfoto</a>";
 
 $queuedir='photos/queue/'; // Path to the queue directory. Remember the trailing slash.
 
@@ -10,6 +10,6 @@ $footer='Powered by Pygmyfoto. <a href="rss.php">RSS feed</a>';
 
 $baseurl="http://dmpop.dyndns.org/pygmyfoto/"; // Remember the trailing slash
 
-$password="password" // For use with the publish.php and unpublish.php scripts
+$password="m0nk3y" // For use with the publish.php and unpublish.php scripts
 
 ?> 
